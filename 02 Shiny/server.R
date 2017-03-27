bd38f8418fd3b5a7e6e62c975c1d1a96b5e46403
ASUS\Franny <frannyfang@gmail.com>
2017-03-27 10:57:49 -0500

    no message

--- 02 Shiny/server.R
+++ 02 Shiny/server.R
@@ -7,13 +7,12 @@ require(RCurl)
 require(grid)
 
 # call in data from data.world
-df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ",'oraclerest.cs.utexas.edu:5000/rest/native/?query="select * from pantheria"')),httpheader=c(DB='jdbc:data:world:sql:ehjkim:s-17-dv-project-4', USER='ehjkim', PASS=redacted , MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ",'oraclerest.cs.utexas.edu:5060/rest/native/?query="select * from Pantheria"')),httpheader=c(DB='jdbc:data:world:sql:ehjkim:s-17-dv-project-4', USER='franny', PASS=redacted , MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))
 
-# df <- read.csv("https://query.data.world/s/dqctnno3ugvf2y4grf7v0ftj9",header=T);
 
 server <- function(input, output) {
   output$plot1 <- renderPlot({
-    ggplot(df) + geom_point(aes(x=X17.1_MaxLongevity_m, y=X23.1_SexualMaturityAge_d, color=MSW05_Order, size=4)) + guides(size=FALSE) # guides is the legend, cuz size will become a legend, so you wanna turn it off
+    ggplot(df) + geom_point(aes(x=v_22, y=v_27, color=v_0, size=4)) + guides(size=FALSE) + labs(x="Max Longevity (months)", y="Sexual Maturity Age (days)") + scale_color_discrete (guide=guide_legend(title = "Order"))
   })
   
   output$plot2 <- renderPlot({
@@ -21,7 +20,7 @@ server <- function(input, output) {
     bdf=brushedPoints(df, input$plot_brush)
     # View(bdf)
     if( !is.null(input$plot_brush) ) {
-      df %>% dplyr::filter(X17.1_MaxLongevity_m %in% bdf[, "X17.1_MaxLongevity_m"]) %>% ggplot() + geom_point(aes(x=X17.1_MaxLongevity_m, y=X23.1_SexualMaturityAge_d, color=MSW05_Order, size=4)) + guides(size=FALSE)
+      df %>% dplyr::filter(v_22 %in% bdf[, "v_22"]) %>% ggplot() + geom_point(aes(x=v_22, y=v_27, color=v_0, size=4)) + guides(size=FALSE) + labs(x="Max Longevity (months)", y="Sexual Maturity Age (days)") + scale_color_discrete (guide=guide_legend(title = "Order"))
     } 
   })
 }
